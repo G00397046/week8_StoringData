b2a2618058d751448b86282a318bc7e6bb99306b
G00397046 <G00397046@atu.ie>
2022-11-15 13:31:01 +0000

    Week 8 Using File API and its methods

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -6,8 +6,13 @@ import java.io.IOException;
 public class CreateFile {
     public static void main(String[] args) throws IOException {
         File myFile = new File("MyFile.txt");
-        System.out.println("My file is located at " + myFile.getAbsolutePath());
-
+        if(myFile.exists() != true){
+            myFile.createNewFile();
+        }
+        else{
+            System.out.println("A file with the name of " + myFile.getName() + " already exists.");
+        }
+        System.out.println(myFile.getName() + " is located at " + myFile.getAbsolutePath());
 
     }
 }
